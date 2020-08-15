5a6e8a4528ddd12c053568bdcf4e978006fce6c1
Lina261 <alina0206200119@gmail.com>
2020-08-15 14:54:45 +0300

    PART-1

--- src/cukl6.java
+++ src/cukl6.java
@@ -4,8 +4,8 @@ public class cukl6 {
     static void run() {
         for(int a = 1; a<127; a++) {
             String str = Integer.toOctalString(a);
-            int dec = Integer.parseInt(str, 8);
-            System.out.println("Знак ASCII будет " +  (char) dec);
+
+            System.out.println("Знак ASCII будет " +  (char) a);
         }
 }}
 
